f13941747376090deecb0d618d940f5f0d006846
Zhongheng-Cheng <zc2737@columbia.edu>
2023-12-01 00:42:42 -0500

    added receiving image and rebuilding image

--- face_recog_google_cloud/server.py
+++ face_recog_google_cloud/server.py
@@ -1,15 +1,37 @@
 from socket import *
 
+SOI = b'\xff\xd8' # Start of Image
+SOS = b'\xff\xda' # Start of Scan
+EOI = b'\xff\xd9' # End of Image
+
 def receive_data_once():
-    message = connectionSocket.recv(1024)
-    if not message:
+    data = connectionSocket.recv(1024)
+    if not data:
         raise 
-    return message
+    return data
 
 def send_data(message):
     connectionSocket.send(message.encode())
     return
 
+def receive_image():
+    image_bytes = b''
+    while not image_bytes.endswith(EOI):
+        data = receive_data_once()
+        if data.startswith(SOI):
+            image_bytes = b''
+        if data.endswith(EOI):
+            break
+    return image_bytes
+
+
+def rebuild_image(jpeg_bytes, path='downloaded_image.jpg'):
+    from PIL import Image
+    from io import BytesIO
+    image_stream = BytesIO(jpeg_bytes)
+    image = Image.open(image_stream)
+    image.save(path)
+    return
 
 serverPort = int(input("Server port: "))
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -20,9 +42,9 @@ connectionSocket, clientAddress = serverSocket.accept()
 print("Connection established with ", clientAddress)
 while True:
     try:
-        message = receive_data_once()
+        image_bytes = receive_image()
         print("Received...")
-        print(message)
+        print(image_bytes)
         print("===============================")
     except Exception as e:
         print("The server is ready to receive")

7ac10755cc44fd22d2c3bea9afafca98583333bc
Zhongheng Cheng <zc2737@columbia.edu>
2023-12-01 02:57:32 +0000

    added detecting client disconnection

--- face_recog_google_cloud/server.py
+++ face_recog_google_cloud/server.py
@@ -2,6 +2,8 @@ from socket import *
 
 def receive_data_once():
     message = connectionSocket.recv(1024)
+    if not message:
+        raise 
     return message
 
 def send_data(message):
@@ -22,7 +24,7 @@ while True:
         print("Received...")
         print(message)
         print("===============================")
-    except KeyboardInterrupt:
+    except Exception as e:
         print("The server is ready to receive")
         connectionSocket, clientAddress = serverSocket.accept()
         print("Connection established with ", clientAddress)

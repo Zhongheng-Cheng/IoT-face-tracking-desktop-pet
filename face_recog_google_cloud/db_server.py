52db77ea25389db3d98d2e0acfb73dae58f946ea
Zhongheng Cheng <zc2737@columbia.edu>
2023-12-08 01:21:00 +0000

    added database posting method

--- face_recog_google_cloud/db_server.py
+++ face_recog_google_cloud/db_server.py
@@ -3,6 +3,14 @@ import pymongo
 mongo_client = pymongo.MongoClient()
 db = mongo_client.testdb
 
-cursor = db.testdb.find()
-for doc in cursor:
-    print(doc.items())
+def db_get():
+    cursor = db.testdb.find()
+    return list(cursor)
+
+def db_post(doc: dict):
+    db.testdb.insert_one(doc)
+    return
+
+print(db_post({"name": "cccc", "age": 15}))
+
+print(db_get())

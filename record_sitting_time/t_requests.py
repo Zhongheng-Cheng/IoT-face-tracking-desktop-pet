5160c62556bbfcec2e2a9d79e11086e6db14eb13
Zhongheng Cheng <zc2737@columbia.edu>
2023-12-08 16:25:03 +0000

    added example script for posting sitting-time data

---
+++ record_sitting_time/t_requests.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+url = "http://35.232.170.48:8000/sitting-time"
+data = {'start_time': '2021-11-02T12:58:51', 'end_time': '2021-11-02T13:58:51'}
+
+response = requests.post(url=url, params=data)
+
+if response.status_code != 200:
+    print(f"Error: {response.content}")
+    print("Request URL:", response.request.url)
+    print("Request Headers:", response.request.headers)
+    print("Request Body:", response.request.body)

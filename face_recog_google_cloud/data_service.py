82a4d89cd6d19cad4324ebe32757b261a32ad16b
Zhongheng Cheng <zc2737@columbia.edu>
2023-12-08 01:37:10 +0000

    added renamed from db_server.py to data_service.py

---
+++ face_recog_google_cloud/data_service.py
@@ -0,0 +1,18 @@
+import pymongo
+
+class DataService(object):
+    def __init__(self):
+        mongo_client = pymongo.MongoClient()
+        self.db = mongo_client.testdb
+        self.collection = self.db.testdb
+
+    def db_get(self, query: None):
+        if query:
+            cursor = self.collection.find(query)
+        else:
+            cursor = self.collection.find()
+        return list(cursor)
+
+    def db_post(self, doc: dict):
+        self.collection.insert_one(doc)
+        return
